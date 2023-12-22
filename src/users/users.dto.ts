58bc9ea258291e614a9ecf86c5f4ba7c54fa028e
esrafilelahi <essinho1121@gmail.com>
2023-12-22 23:18:44 +0330

    update dto interface file

--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -1,7 +1,10 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class CreateUserDto {
-  @IsString()
+  @IsNotEmpty({ message: 'Name should not be empty' })
+  @IsString({ message: 'Name should be a string' })
+  @MinLength(2, { message: 'Name should be at least 2 characters long' })
+  @MaxLength(20, { message: 'Name should not exceed 20 characters' })
   readonly name: string;
   readonly index?: number;
 }
