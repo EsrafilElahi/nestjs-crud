e4963691cc631f51ef5811480890ffe395856c36
esrafilelahi <essinho1121@gmail.com>
2023-12-22 22:38:26 +0330

    add other controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -8,5 +8,10 @@ export class UsersController {
   @Get()
   getUsers(): string[] {
     return this.usersService.getUsers();
+  }
+
+  @Get(':index')
+  getOne(@Param('index') index: number): string {
+    return this.usersService.getOne(index);
   }
 }
