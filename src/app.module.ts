8eca7ed1a655f3aa12714c6b43641192424a7515
esrafilelahi <essinho1121@gmail.com>
2024-01-04 15:29:42 +0330

    add rate limit

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { ConfigModule } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 import { UserEntity } from './entities/user.entity';
 import config from './config/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -29,9 +31,34 @@ import config from './config/config';
       isGlobal: true, // for use another modules
       cache: true, // as accessing process.env can be slow
     }),
+
+    // rate limit config
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 3,
+      },
+      {
+        name: 'medium',
+        ttl: 10000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {
   constructor(private dataSource: DataSource) {}
