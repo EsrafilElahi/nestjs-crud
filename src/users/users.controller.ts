e1456444deb23059fe7d87817e84f8d391adbfb5
esrafilelahi <essinho1121@gmail.com>
2024-01-04 15:34:09 +0330

    use rate limit throttle in users controller file

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,16 +10,20 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './users.dto';
+import { SkipThrottle, Throttle } from '@nestjs/throttler';
 
+@SkipThrottle()
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @SkipThrottle({ default: false }) // don't skip rate-limit
   @Get()
   getUsers() {
     return this.usersService.getUsers();
   }
 
+  @Throttle({ short: { ttl: 1000, limit: 1 } }) // customize throttle short
   @Get(':id')
   getOne(@Param('id', ParseIntPipe) id: number) {
     return this.usersService.getOne(id);
